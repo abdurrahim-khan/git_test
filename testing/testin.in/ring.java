59ea2893f557835dd566f6f877c9c5f3bef59bf5
abdurrahim-khan <abdurrahimalig@gmail.com>
2022-11-11 16:15:41 +0530

    4th commit

---
+++ testing/testin.in/ring.java
@@ -0,0 +1,10 @@
+package testing.testin.in;
+
+import jdk.internal.cmm.SystemResourcePressureImpl;
+
+public class ring {
+    public void ringing()
+    {
+        System.out.print("ringing");
+    }
+}
